1ce20710d15e616b6062a19806a17c1e4eef7a21
NCIAlexGar <116267528+NCIAlexGar@users.noreply.github.com>
2024-01-25 10:46:36 +0000

    Add files via upload

---
+++ Exercise3/src/exercise3/CarApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise3;
+
+/**
+ *
+ * @author Alex
+ */
+public class CarApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        CarGUI myGUI = new CarGUI();
+        myGUI.setVisible(true);
+    }
+    
+}
